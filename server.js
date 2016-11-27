acdfe1d25651e63879e06abb27345364371bec8b
Leif <MageLeif@Yahoo.com>
2016-11-27 16:39:38 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.use("/static", express.static(__dirname + "/static")); // Define static directory
+app.use(express.static(__dirname + "/html"));              // Root goes to the /html directoru
+
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname + "/html/404.html"), 404);
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(8087);
