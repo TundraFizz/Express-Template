c4588fa92f8b58a5895e1cabec33cca395e6a02c
Leif <MageLeif@Yahoo.com>
2016-11-28 12:02:13 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,10 +12,14 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080 # Redirect port 80 to 8080
 sudo iptables-save                                                              # Save the IP tables
 
-mkdir myapp                # Make a directory for your application
-cd myapp                   # Go into the directory
-npm install express --save # Install Express in the myapp directory and save it in the dependencies list
-node server.js             # Start up the server
+# git clone https://github.com/TundraFizz/NodeJS-Template.git # Clone from repository
+# cd NodeJS-Template Go into the directory
+npm install
+node server.js
+
+# OLD
+# npm install express --save # Install Express in the myapp directory and save it in the dependencies list
+# node server.js             # Start up the server
 
 #########
 # NGINX #
