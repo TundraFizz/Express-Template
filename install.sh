78a9c229662157d259998d73da1490fdb79e1cff
Leif <MageLeif@Yahoo.com>
2016-11-28 11:38:58 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+#############
+# LOOK HERE #
+###################################################
+# https://nodejs.org/en/download/package-manager/ #
+###################################################
+
+sudo yum -y update
+sudo yum -y install gcc
+curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
+# Logout and then log back in
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080 # Redirect port 80 to 8080
+sudo iptables-save                                                              # Save the IP tables
+
+mkdir myapp                # Make a directory for your application
+cd myapp                   # Go into the directory
+npm install express --save # Install Express in the myapp directory and save it in the dependencies list
+node server.js             # Start up the server
+
+#########
+# NGINX #
+#########
+
+sudo yum -y install nginx            # Install NGINX
+sudo chmod 777 /etc/nginx/nginx.conf # Give user permission to access nginx.conf
+sudo nano /etc/nginx/nginx.conf      # Modify nginx.conf (example below)
+sudo nginx                           # Start up NGINX
+
+####################
+# NGINX Management #
+####################
+# sudo nginx -s reload                                       # Do this command whenever you edit /etc/nginx/nginx.conf
+# sudo systemctl restart nginx                               # This restarts nginx
+# sudo top -c -p $(pgrep nginx | tr '\n' ',' | sed 's/.$//') # Force stop nginx
