40e6dde0bf4221cd2534b54d9f01b97b27440f2e
Tundra Fizz <mageleif@yahoo.com>
2016-11-29 16:24:14 -0600

    Update files

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+var fs = require("fs");                         // File system library
 var express = require("express");               // Require express
 var app = express();                            // Define app
 app.use(express.static(__dirname + "/static")); // Define the static directory
 app.set("views", __dirname + "/views");         // Define the views directory
 app.set("view engine", "ejs");                  // Set the view engine to ejs
 
-// POST
+/////////////////////
+// POST: customers //
+/////////////////////
 app.post('/customers', function(req, res) {
   var customers = [{firstName:'Peter', lastName: 'Pan', age:13},{firstName:'Captain', lastName:'Hook', age:35}];
   res.json(customers);
 });
 
-// Index
+///////////
+// Index //
+///////////
 app.get("/", function(req, res) {
   var people = [
     {name: "Adam", age: 10},
@@ -25,16 +30,38 @@ app.get("/", function(req, res) {
   });
 });
 
-// About
+///////////
+// About //
+///////////
 app.get("/about", function(req, res) {
-  var people = [{"firstName": "a", "lastName": "b", "age": 12},
-                {"firstName": "c", "lastName": "d", "age": 34}];
+  var people = CsvToObject("data/people.csv");
   res.render("pages/about.ejs", {people: people});
 });
 
-// 404: No route or file exists
+//////////////////////////////////
+// 404: No route or file exists //
+//////////////////////////////////
 app.use(function (req, res) {
   res.render("pages/404.ejs");
 });
 
 app.listen(9001);
+
+//////////////////////
+// Helper Functions //
+//////////////////////
+
+function CsvToObject(file) {
+  var people = [];
+  var fileContents = fs.readFileSync(file);
+  var lines = fileContents.toString().split("\n");
+
+  for(var i = 0; i < lines.length - 1; i++) {
+    var data = lines[i].split(",");
+    people.push({"firstName": data[0],
+                 "lastName":  data[1],
+                 "age":       data[2],});
+  }
+
+  return people;
+}
