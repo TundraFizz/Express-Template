11e567b9824210584a4eed6034a0fa43022501fa
Leif Coleman <mageleif@yahoo.com>
2016-12-02 13:46:05 -0600

    Update files

--- install.sh
+++ install.sh
@@ -1,36 +1,61 @@
-#############
-# LOOK HERE #
-###################################################
-# https://nodejs.org/en/download/package-manager/ #
-###################################################
+inquire(){
+  run=true
+  while [ $run = true ]; do
+    echo -n "$1 [y/n] "
+    read answer
+    answer=$(echo $answer | tr '[:upper:'] '[:lower:]')
+
+    case $answer in
+      "y" | "yes")
+        return 1;;
+      "n" | "no")
+        return 0;;
+      *);;
+    esac
+  done
+}
 
 sudo yum -y update                                # Update the machine
 sudo yum -y install gcc                           # Install GCC
 
 sudo yum -y install nodejs npm --enablerepo=epel  # Install nodejs and npm
-# OLD (requires you to logout and back in which is ANNOYING!)
-# curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash
 
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000 # Redirect port 80 to 9000
-sudo iptables-save                                                              # Save the IP tables
-# ^ This doesn't actually save it for when the machine restarts...
-#   ...so I must find a different solution!
+# Ask the user if they are going to do an advanced install with NGINX
+# If yes, redirect port 80 -> 9000
+# If no, redirect port 80 -> 9001
+echo "Advanced install with NGINX? If you"
+echo "don't know what to pick, choose no"
+inquire "Yes or No"
+nginx=$?
+
+if [[ $nginx == 1 ]]; then
+  # Redirect port 80 to 9000
+  sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9000
+else
+  # Redirect port 80 to 9001
+  sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 9001
+fi
 
 sudo npm install nodemon -g  # Install nodemon globally
 npm install                  # Install the project
 
+echo "Installation complete! You may now use"
+echo "one of the two commands to run your application:"
+echo "nohup node server.js &"
+echo "nodemon server.js"
+
 # Choose one of the two
-node server.js               # PRODUCTION: Start the server (nohup)
-nodemon server.js            # DEVELOPMENT: The server will automatically restart when changes are made
+# nohup node server.js &       # PRODUCTION: Start the server (nohup)
+# nodemon server.js            # DEVELOPMENT: The server will automatically restart when changes are made
 
 #########
 # NGINX #
 #########
 
-sudo yum -y install nginx            # Install NGINX
-sudo chmod 777 /etc/nginx/nginx.conf # Give user permission to access nginx.conf
-sudo nano /etc/nginx/nginx.conf      # Modify nginx.conf (example below)
-sudo nginx                           # Start up NGINX
+# sudo yum -y install nginx            # Install NGINX
+# sudo chmod 777 /etc/nginx/nginx.conf # Give user permission to access nginx.conf
+# sudo nano /etc/nginx/nginx.conf      # Modify nginx.conf (example below)
+# sudo nginx                           # Start up NGINX
 
 ####################
 # NGINX Management #
